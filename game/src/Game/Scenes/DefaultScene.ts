719e8a4c1f08c9db686ff26566cfec6d342cdd52
Borut <bobalazek124@gmail.com>
2020-01-11 08:27:30 +0100

    Physics are now enabled

--- game/src/Game/Scenes/DefaultScene.ts
+++ game/src/Game/Scenes/DefaultScene.ts
@@ -1,4 +1,5 @@
 import * as BABYLON from 'babylonjs';
+import * as Ammo from 'ammo.js';
 
 import { GameManager } from '../../Framework/Core/GameManager';
 import { AbstractNetworkScene } from '../../Framework/Scenes/AbstractNetworkScene';
@@ -11,6 +12,11 @@ export class DefaultScene extends AbstractNetworkScene {
 
     // Prepare scene
     this.scene = new BABYLON.Scene(GameManager.engine);
+    this.scene.enablePhysics(
+      new BABYLON.Vector3(0, -9.82, 0),
+      new BABYLON.AmmoJSPlugin(true, Ammo.default() /* hack */)
+    );
+
     this.prepareCamera();
     this.prepareLights();
     this.prepareEnvironment();

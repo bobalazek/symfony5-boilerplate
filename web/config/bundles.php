ad93989f50bf9d6d0b0609d2de28f3fdd3fee0f0
Borut <bobalazek124@gmail.com>
2020-02-25 18:46:36 +0100

    remove unused suff

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -19,7 +19,5 @@ return [
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];

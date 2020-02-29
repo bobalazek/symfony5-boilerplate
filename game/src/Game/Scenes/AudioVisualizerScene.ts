3705da8d6b67966c2ffd1b61a70db046ef764876
Borut <bobalazek124@gmail.com>
2020-02-29 19:31:00 +0100

    audio visualiser stuff

--- game/src/Game/Scenes/AudioVisualizerScene.ts
+++ game/src/Game/Scenes/AudioVisualizerScene.ts
@@ -1,9 +1,15 @@
 import {
   Vector3,
+  Color3,
   Mesh,
+  Sound,
+  Engine,
+  AudioEngine,
+  Analyser,
   VertexData,
-  StandardMaterial,
+  VertexBuffer,
   FreeCamera,
+  StandardMaterial,
   HemisphericLight,
 } from 'babylonjs';
 
@@ -42,37 +48,117 @@ export class AudioVisualizerScene extends AbstractScene {
   }
 
   prepareLights() {
-    new HemisphericLight(
-      'light',
+    let hemisphericLight = new HemisphericLight(
+      'hemisphericLight',
       Vector3.Up(),
       this.babylonScene
     );
   }
 
   prepareVisualizer() {
-    let visualizer = new Mesh('visualizer', this.babylonScene);
-    let visualizerMaterial = new StandardMaterial('visualizerMaterial', this.babylonScene);
-    visualizerMaterial.wireframe = true;
-    visualizer.material = visualizerMaterial;
+    const name = 'visualizer';
+    const segments = 32;
+    const diameter = 5;
+    const audioMultiplier = 0.01;
+    let mesh = this.generatePolyMesh(
+      name,
+      segments,
+      diameter
+    );
 
-    // TODO
-    let positions = [
-      -5, 2, -3,
-      -7, -2, -3,
-      -3, -2, -3,
-      5, 2, 3,
-      7, -2, 3,
-      3, -2, 3,
-    ];
-    let indices = [
-      0, 1, 2, 3, 4, 5,
-    ];
+    var track = new Sound(
+      'track',
+      '/static/audio/track01.mp3',
+      this.babylonScene,
+      () => {
+        track.play();
+      },
+      {
+        loop: true,
+        autoplay: true,
+      }
+    );
+
+    let analyser = new Analyser(this.babylonScene);
+    (<AudioEngine>Engine.audioEngine).connectToAnalyser(analyser); // TODO: fix when PR is merged
+    analyser.FFT_SIZE = segments * 2;
+    //analyser.SMOOTHING = 0.9;
+
+    this.babylonScene.registerBeforeRender(() => {
+	    const workingArray = analyser.getByteFrequencyData();
+
+      let diameterAddArray = [];
+      for (let i = 0; i < analyser.getFrequencyBinCount(); i++) {
+        diameterAddArray[i] = workingArray[i] * audioMultiplier;
+      }
+
+      const vertexData = this.generatePolyMeshVertexData(segments, diameter, diameterAddArray);
+      vertexData.applyToMesh(mesh, true);
+  	});
+  }
+
+  generatePolyMesh(name: string = 'shape', segments: number = 32, diameter: number = 5): Mesh {
+    let mesh = new Mesh(
+      name,
+      this.babylonScene
+    );
+    let meshMaterial = new StandardMaterial(
+      name + 'Material',
+      this.babylonScene
+    );
+    meshMaterial.backFaceCulling = false;
+    meshMaterial.emissiveColor = new Color3(1, 1, 1);
+    mesh.material = meshMaterial;
+
+    const vertexData = this.generatePolyMeshVertexData(segments, diameter);
+    vertexData.applyToMesh(mesh, true);
+
+    mesh.rotate(new Vector3(1, 0, 0), Math.PI);
+
+    return mesh;
+  }
+
+  generatePolyMeshVertexData(segments: number, diameter: number, diameterAddArray?: Array<number>) {
+    const positionsCount = segments + 2;
+    const segmentWidth = Math.PI /* * 2 */ / segments;
 
     let vertexData = new VertexData();
+    let positions = [];
+    let indices = [];
+    let colors = [];
+    let normals = [];
+    let angle = 0;
+
+    // Calculate
+    positions.push(0, 0, 0);
+    colors.push(1, 0, 0, 1);
+
+    for (let i = 1; i < positionsCount; i++) {
+      let add = diameterAddArray && typeof diameterAddArray[i-2] !== 'undefined'
+        ? diameterAddArray[i-2]
+        : 0;
+
+      const finalDiameter = diameter + add;
+
+      const x = Math.cos(angle) * finalDiameter;
+      const y = Math.sin(angle) * finalDiameter;
+
+      positions.push(x, y, 0);
+      colors.push(1, 0, 0, 1);
+
+      angle -= segmentWidth;
+      if (i > 1) {
+        indices.push(0, i - 1, i);
+      }
+    }
+
+    VertexData.ComputeNormals(positions, indices, normals);
 
     vertexData.positions = positions;
     vertexData.indices = indices;
+    vertexData.colors = colors;
+    vertexData.normals = normals;
 
-    vertexData.applyToMesh(visualizer);
+    return vertexData;
   }
 }
--- game/src/Game/index.ts
+++ game/src/Game/index.ts
@@ -1,4 +1,6 @@
 import { GameManager } from '../Framework/Core/GameManager';
+
+/*
 import { ThirdPersonController } from '../Framework/Gameplay/Controller';
 import { ThirdPersonInputBindings } from '../Framework/Gameplay/InputBindings';
 import { DefaultScene } from './Scenes/DefaultScene';
@@ -10,4 +12,16 @@ GameManager.boot({
   controller: ThirdPersonController,
   inputBindings: ThirdPersonInputBindings,
   defaultScene: DefaultScene,
+});
+*/
+
+import { AbstractController } from '../Framework/Gameplay/Controller';
+import { AudioVisualizerScene } from './Scenes/AudioVisualizerScene';
+
+GameManager.boot({
+  engineOptions: {
+    stencil: true,
+  },
+  controller: AbstractController,
+  defaultScene: AudioVisualizerScene,
 });

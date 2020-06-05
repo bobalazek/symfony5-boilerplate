80686ebf43154bc2a15d6f94c9c63f460078b36a
Borut <bobalazek124@gmail.com>
2020-06-05 07:09:17 +0200

    php cs fix

--- web/src/Kernel.php
+++ web/src/Kernel.php
@@ -14,15 +14,15 @@ class Kernel extends BaseKernel
     protected function configureContainer(ContainerConfigurator $container): void
     {
         $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
         $container->import('../config/{services}.yaml');
-        $container->import('../config/{services}_'.$this->environment.'.yaml');
+        $container->import('../config/{services}_' . $this->environment . '.yaml');
         $container->import('../config/{data}/*.yaml');
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
         $routes->import('../config/{routes}.yaml');
     }

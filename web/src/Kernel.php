b6efb65829a42f708f89b4f7ac426b60af15cc7b
Borut <bobalazek124@gmail.com>
2020-06-14 11:32:52 +0200

    data to parameters

--- web/src/Kernel.php
+++ web/src/Kernel.php
@@ -13,11 +13,11 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
+        $container->import('../config/{parameters}/*.yaml');
         $container->import('../config/{packages}/*.yaml');
         $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
         $container->import('../config/{services}.yaml');
         $container->import('../config/{services}_' . $this->environment . '.yaml');
-        $container->import('../config/{data}/*.yaml');
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void

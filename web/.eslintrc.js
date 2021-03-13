435a5df8806a97a85bd28bc2f853b022045b435d
Borut Balazek <bobalazek124@gmail.com>
2021-03-13 07:17:21 +0100

    Slight tweaks

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 11,
-        sourceType: 'module',
-    },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
 };

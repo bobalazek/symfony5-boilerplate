8b0eebfc246365dd41c66cd2a2be9f53ab8c1555
Ana <anakociper124@gmail.com>
2021-07-12 17:04:34 +0200

    Added commands and settings tests

--- e2e/cypress/integration/settings.spec.js
+++ e2e/cypress/integration/settings.spec.js
@@ -10,11 +10,11 @@ describe('Settings', () => {
 
     cy.visit('/settings');
 
-    cy.get('input[name="settings[firstName]"]').clear().type('User2');
+    cy.getInputByLabel('First name').clear().type('User2');
 
-    cy.get('button[type="submit"]').click();
+    cy.contains('Save').click();
 
-    cy.get('input[name="settings[firstName]"]').should('have.value', 'User2');
+    cy.getInputByLabel('First name').should('have.value', 'User2');
   });
 
   it('Checking if image settings are saved correctly', () => {
@@ -34,11 +34,11 @@ describe('Settings', () => {
 
     cy.visit('/settings/password');
 
-    cy.get('input[name="settings_password[oldPlainPassword]"]').type('password');
-    cy.get('input[name="settings_password[plainPassword][first]"]').type('newpassword');
-    cy.get('input[name="settings_password[plainPassword][second]"]').type('newpassword');
+    cy.getInputByLabel('Old password').type('password');
+    cy.getInputByLabel('New password').type('newpassword');
+    cy.getInputByLabel('Repeat new password').type('newpassword');
 
-    cy.get('button[type="submit"]').click();
+    cy.contains('Save').click();
 
     cy.get('div.alert.alert-success').contains('successfully');
   });
@@ -50,11 +50,11 @@ describe('Settings', () => {
 
     cy.visit('/settings/password');
 
-    cy.get('input[name="settings_password[oldPlainPassword]"]').type('superwrongpassword');
-    cy.get('input[name="settings_password[plainPassword][first]"]').type('newpassword');
-    cy.get('input[name="settings_password[plainPassword][second]"]').type('newpassword');
+    cy.getInputByLabel('Old password').type('superwrongpassword');
+    cy.getInputByLabel('New password').type('newpassword');
+    cy.getInputByLabel('Repeat new password').type('newpassword');
 
-    cy.get('button[type="submit"]').click();
+    cy.contains('Save').click();
 
     cy.get('.form-error-message').contains('Wrong value for your current password');
   });
@@ -71,6 +71,10 @@ describe('Settings', () => {
       .find('a.btn.btn-confirm')
       .click();
 
+      cy.on('window:confirm', (text) => {
+        expect(text).to.contains('Are you sure?');
+      });
+
     cy.url().should('include', '/auth/login');
   });
 
@@ -79,10 +83,67 @@ describe('Settings', () => {
 
     cy.visit('/settings/tfa');
 
-    cy.get('select[name="settings_tfa[tfaEnabled]"]').select('1', { force: true });
+    cy.get('select[name="settings_tfa[tfaEnabled]"]').select('Enabled', { force: true });
 
-    cy.get('button[type="submit"]').click();
+    cy.contains('Save').click();
 
     cy.get('.alert.alert-danger').should('exist').contains('You enabled 2FA, but do not have any methods enabled. Please activate at least one method for the functionality to work');
+  });
+
+  it('Checking if block users are shown', () => {
+    // No blocked users
+    cy.login();
+
+    cy.visit('/settings/blocks');
+
+    cy.contains('You are not blocking anyone yet');
+
+    // With blocked user
+    cy.visit('/users/ana')
+
+    cy.get('a').contains(' Block ').click();
+
+    cy.get('a').contains('user').click();
+
+    cy.get('.dropdown-menu').contains('Settings').click();
+
+    cy.get('li a').contains('Blocks').click();
+
+    cy.get('.card .card-body small').contains('(ana)').should('exist');
+
+    // Unblock blocked user
+    cy.get('.card .card-body small').contains('(ana)').closest('.card').contains('View').click();
+
+    cy.get('a').contains(' Unblock ').click();
+
+    cy.contains('You have successfully unblocked the user').should('exist');
+
+    cy.visit('/settings/blocks').contains('You are not blocking anyone yet');
+  });
+
+  it('Checking if export request works', () => {
+    cy.login();
+
+    cy.visit('/settings/export');
+
+    cy.contains('Here you can request an export of all your data on this platform');
+
+    cy.contains('Request export').click();
+
+    cy.contains('Export successfully requested');
+
+    cy.wait(2000);
+
+    cy.reload();
+
+    cy.get('a').contains('Download').should('exist');
+  });
+
+  it('Checking if alert message in deletion exist', () => {
+    cy.login();
+
+    cy.visit('/settings/deletion');
+
+    cy.contains('This action is permanent. You will not be able to retrieve any of the content or information you shared with this platform. An email will be sent to you, so you can confirm your action').should('exist');
   });
 });
--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -6,8 +6,12 @@ Cypress.Commands.add('login', (username = 'user', password = 'password') => {
   cy.get('input[name="password"]').type(password);
 
   cy.get('button[type="submit"]').click();
-})
+});
 
 Cypress.Commands.add('logout', () => {
   cy.visit('/auth/logout');
-})
+});
+
+Cypress.Commands.add('getInputByLabel', (label) => {
+  return cy.get('label').contains(label).parent().find('.form-control');
+});

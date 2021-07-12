2f63bd2512f5e143b0976abffb6c2d20850566ac
Ana <anakociper124@gmail.com>
2021-07-12 09:30:53 +0200

    Added messaging and user tests

---
+++ e2e/cypress/integration/messaging.spec.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+describe('Users', () => {
+    before(() => {
+        cy.task('database:recreate');
+    });
+
+    it('Checking if I can message another user', () => {
+        // Send message
+        cy.login();
+
+        cy.visit('/users/ana');
+
+        cy.get('a').contains(' Message ').click();
+
+        cy.get('#messaging-threads a.messaging-thread:first').click();
+
+        cy.contains('This thread has no messages yet').should('exist');
+
+        cy.get('#messaging-thread-messages-wrapper .form-control').type('Hello World');
+
+        cy.get('#messaging-thread-messages-wrapper').contains('Send').click();
+
+        cy.contains('This thread has no messages yet').should('not.exist');
+
+        // Recieve message
+        cy.logout();
+
+        cy.login('ana');
+
+        cy.get('header .dropdown .dropdown-toggle').click();
+
+        cy.get('header .dropdown-menu').contains('Messaging').click();
+
+        cy.contains('No thread selected').should('exist');
+
+        cy.get('#messaging-threads a.messaging-thread:first').click();
+
+        cy.get('#messaging-thread-messages-inner').contains('Hello World').should('exist');
+    });
+  });
--- e2e/cypress/integration/users.spec.js
+++ e2e/cypress/integration/users.spec.js
@@ -85,39 +85,4 @@ describe('Users', () => {
 
         cy.contains('You are blocked by this user').should('not.exist');
     });
-
-    it.only('Checking if I can message another user', () => {
-        // Send message
-        cy.login();
-
-        cy.visit('/users/ana');
-
-        cy.get('a').contains(' Message ').click();
-
-        cy.get('#messaging-threads a.messaging-thread:first').click();
-
-        cy.contains('This thread has no messages yet').should('exist');
-
-        cy.get('#messaging-thread-messages-wrapper .form-control').type('Hello World');
-
-        cy.get('#messaging-thread-messages-wrapper').contains('Send').click();
-
-        cy.contains('This thread has no messages yet').should('not.exist');
-
-        // Recieve message
-        cy.logout();
-
-        cy.login('ana');
-
-        cy.get('header .dropdown .dropdown-toggle').click();
-
-        cy.get('header .dropdown-menu').contains('Messaging').click();
-
-        cy.contains('No thread selected').should('exist');
-
-        cy.get('#messaging-threads a.messaging-thread:first').click();
-
-        cy.get('#messaging-thread-messages-inner').contains('Hello World').should('exist');
-
-    });
   });

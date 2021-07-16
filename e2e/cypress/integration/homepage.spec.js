7e4b3718001ea9744da4827e9710b170cb40aa8e
Ana <anakociper124@gmail.com>
2021-07-16 10:55:17 +0200

    Added homepage tests

--- e2e/cypress/integration/homepage.spec.js
+++ e2e/cypress/integration/homepage.spec.js
@@ -6,4 +6,14 @@ describe('Homepage', () => {
 
       cy.get('h1').contains('home.heading');
   });
+
+  it.only('Checking if footer language dropdown works', () => {
+    cy.visit('/');
+
+    cy.get('.page-footer .dropdown .dropdown-toggle').contains('English').click();
+
+    cy.get('.page-footer .dropdown .dropdown-menu').contains('Deutsch').click();
+
+    cy.get('.page-footer .dropdown .dropdown-toggle').contains('Deutsch').should('exist');
+  });
 });

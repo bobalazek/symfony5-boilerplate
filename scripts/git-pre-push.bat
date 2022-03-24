40037bc8ade519bcf72a6314827c04cb66eede40
Borut Balazek <bobalazek124@gmail.com>
2022-03-24 18:57:49 +0100

    Tweaks

--- scripts/git-pre-push.bat
+++ scripts/git-pre-push.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
 
 ECHO ========== Start running JS lint ... ==========
-docker exec -ti mnc_node yarn run lint
+docker exec -ti s5bp_node yarn run lint
 
 ECHO ========== Start running PHP lint ... ==========
-docker exec -ti mnc_php composer run-script lint
+docker exec -ti s5bp_php composer run-script lint
 
 ECHO ========== Start running PHP tests ... ==========
-docker exec -ti mnc_php composer run-script test
+docker exec -ti s5bp_php composer run-script test
 
 ECHO ========== Start running Cypress tests ... ==========
 docker exec -ti s5bp_cypress yarn test"
--- scripts/git-pre-push.sh
+++ scripts/git-pre-push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo -e "========== Start running JS lint ... =========="
-docker exec -ti mnc_node yarn run lint
+docker exec -ti s5bp_node yarn run lint
 
 echo -e "========== Start running PHP lint ... =========="
-docker exec -ti mnc_php composer run-script lint
+docker exec -ti s5bp_php composer run-script lint
 
 echo -e "========== Start running PHP tests ... =========="
-docker exec -ti mnc_php composer run-script test
+docker exec -ti s5bp_php composer run-script test
 
 echo -e "========== Start running Cypress tests ... =========="
 docker exec -ti s5bp_cypress yarn test"
